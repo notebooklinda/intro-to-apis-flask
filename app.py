3cc78ee6e807f66b34ef9cb5587bde7af3218f44
Mo Li <notebooklinda1990@gmail.com>
2021-01-17 14:50:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     url_for,
 )
+from twilio.rest import Client
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,12 +19,12 @@ TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
 
 def get_sent_messages():
     # TODO: Make this return a collection of messages that were sent from the number
-    messages = []
+    messages = client.messages.list(from_=TWILIO_PHONE_NUMBER)
     return messages
 
 def send_message(to, body):
     # TODO: Send the text message
-    pass
+    client.messages.create(to=to, body=body, from_=TWILIO_PHONE_NUMBER)
 
 @app.route("/", methods=["GET"])
 def index():
